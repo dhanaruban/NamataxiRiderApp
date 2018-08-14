f2b72e8b5e362ac04d37a1756853c7a13886b713
dhanaruban <32019096+dhanaruban@users.noreply.github.com>
2018-08-14 13:50:39 +0530

    firstcommit

---
+++ app/src/main/java/com/example/thenu/androidriderapp/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.thenu.androidriderapp.Common;
+
+public class Common {
+    public  static final String driver_tb1 = "Drivers";
+    public  static final String user_driver_tb1 = "DriversInformation";
+    public  static final String user_rider_tb1 = "RidersInformation";
+    public  static final String pickup_request_tb1 = "PickupRequest";
+
+}
